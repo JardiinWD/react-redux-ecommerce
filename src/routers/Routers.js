2d0dff58a0fc253c58b6ad80e7f93979855e197a
Alessandro Pecorilla <Pecorillaalessandro@gmail.com>
2022-10-22 13:05:08 +0200

    #3 - Created the first pages of the project. Implemented the initial router file.

---
+++ src/routers/Routers.js
@@ -0,0 +1,17 @@
+import React from 'react'
+//Import delle mie pagine
+import Home from '../pages/Home'
+import Cart from '../pages/Cart'
+import Checkout from '../pages/Checkout'
+import Login from '../pages/Login'
+import ProductDetail from '../pages/ProductDetail'
+import Shop from '../pages/Shop'
+import Signup from '../pages/Signup'
+
+const Router = () => {
+    return (
+        <div>Router</div>
+    )
+}
+
+export default Router
