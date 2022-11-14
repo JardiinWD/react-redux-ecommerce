2a658fb1c637c230cc0ce9539275a0a56eba84ba
Alessandro Pecorilla <Pecorillaalessandro@gmail.com>
2022-11-14 18:23:01 +0100

    #30 - Created the login/signup components and their styles. I have to implement the logic

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import React, {useState} from "react";
+import Helmet from '../components/Helmet/Helmet'
+import { Container, Row, Col, Form, FormGroup } from "reactstrap";
+import { Link } from "react-router-dom";
+import '../styles/login.css'
 
 const Login = () => {
-   return <div>Login</div>;
+
+   // useState for the email field
+   const [email, setEmail] = useState('')
+   // useState for the password field
+   const [password, setPassword] = useState('')
+
+   return (
+      /* Login */
+      <Helmet title='Login'>
+         <section>
+            {/* Container */}
+            <Container>
+               {/* Row */}
+               <Row>
+                  {/* lg='6' */}
+                  <Col lg='6' className="m-auto text-center">
+                     {/* Login Title */}
+                     <h3 className="fw-bold mb-4">Login</h3>
+                     {/* auth_form */}
+                     <Form className="auth_form">
+                        {/* form_group | Email */}
+                        <FormGroup className="form_group">
+                           <input type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
+                        </FormGroup>
+                        {/* form_group | Email */}
+                        <FormGroup className="form_group">
+                           <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+                        </FormGroup>
+                        {/* buy_btn */}
+                        <button type="submit" className="buy_btn login_btn">
+                           Login
+                        </button>
+                        {/* Don't have an Account */}
+                        <p>
+                           Don't have an account? 
+                           <Link to='/signup'>Create an account</Link>
+                        </p>
+                     </Form>
+                  </Col>
+               </Row>
+            </Container>
+         </section>
+      </Helmet>
+   );
 };
 
 export default Login;
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,81 @@
-import React from "react";
+import React, {useState} from "react";
+import Helmet from '../components/Helmet/Helmet'
+import { Container, Row, Col, Form, FormGroup } from "reactstrap";
+import { Link } from "react-router-dom";
+import '../styles/login.css'
 
 const Signup = () => {
-   return <div>Signup</div>;
+
+   //FIXME: Think about all these state, use the useReducer instead!
+
+   // useState for the username field
+    const [username, setUsername] = useState('')  
+   // useState for the email field
+   const [email, setEmail] = useState('')
+   // useState for the password field
+   const [password, setPassword] = useState('')
+   // useState for the password field
+   const [file, setFile] = useState(null)
+
+   const submitHandler = (e) => {
+      // Prevent refresh on submit
+      e.preventDefault()
+      // Clear all fields
+      setUsername('')
+      setEmail('')
+      setPassword('')
+      if (file !== null) setFile(null)
+   }
+
+
+   return (
+      /* Signup */
+      <Helmet title='Signup'>
+         <section>
+            {/* Container */}
+            <Container>
+               {/* Row */}
+               <Row>
+                  {/* lg='6' */}
+                  <Col lg='6' className="m-auto text-center">
+                     {/* Signup Title */}
+                     <h3 className="fw-bold mb-4">Signup</h3>
+                     {/* auth_form */}
+                     <Form className="auth_form" onSubmit={submitHandler}>
+                        {/* form_group | Username */}
+                        <FormGroup className="form_group">
+                           <input type="text" placeholder="Username" value={username} onChange={e => setUsername(e.target.value)} required />
+                        </FormGroup>
+                        {/* form_group | Email */}
+                        <FormGroup className="form_group">
+                           <input type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} required />
+                        </FormGroup>
+                        {/* form_group | Password */}
+                        <FormGroup className="form_group">
+                           <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} required />
+                        </FormGroup>
+                        {/* form_group | Password */}
+                        <FormGroup className="form_group">
+                           <input type="file" onChange={e => setFile(e.target.files[0])} required />
+                        </FormGroup>
+
+
+                        {/* buy_btn */}
+                        <button type="submit" className="buy_btn login_btn">
+                           Create an account
+                        </button>
+                        {/* Already have an Account */}
+                        <p>
+                           Already have an account? 
+                           <Link to='/login'>Login</Link>
+                        </p>
+                     </Form>
+                  </Col>
+               </Row>
+            </Container>
+         </section>
+      </Helmet>
+   );
 };
 
 export default Signup;
